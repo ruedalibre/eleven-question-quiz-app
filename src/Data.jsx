be103197c2e6060b8a72178b37ae81879c53582d
ruedalibre <andresruedalibre@gmail.com>
2022-03-29 09:17:36 -0500

    new project

---
+++ src/Data.jsx
@@ -0,0 +1,114 @@
+const data = {
+    "response_code":0,
+    "results": [
+        {
+            "category":"General Knowledge",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"This is the correct spelling of &quot;Supercalifragilisticexpialidocious&quot;.",
+            "correct_answer":"True",
+            "incorrect_answers":[
+                "False"
+            ]
+        },
+        
+        {
+            "category":"Entertainment: Video Games",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"Unturned originally started as a Roblox game.",
+            "correct_answer":"True",
+            "incorrect_answers":[
+                "False"
+            ]
+        },
+        
+        {
+            "category":"History",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"The fourth funnel of the RMS Titanic was fake designed to make the ship look more powerful and symmetrical.",
+            "correct_answer":"True",
+            "incorrect_answers":[
+                "False"
+            ]
+        },
+        
+        {
+            "category":"Entertainment: Music",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"The song &quot;Mystery Train&quot; was released by artist &quot;Little Junior&#039;s Blue Flames&quot; in 1953.",
+            "correct_answer":"True",
+            "incorrect_answers":[
+                "False"
+            ]
+        },
+        
+        {
+            "category":"History",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"The Battle of Trafalgar took place on October 23rd, 1805",
+            "correct_answer":"False",
+            "incorrect_answers":[
+                "True"
+            ]
+        },
+        
+        {
+            "category":"Entertainment: Books",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"Harry Potter was born on July 31st, 1980.",
+            "correct_answer":"True",
+            "incorrect_answers":[
+                "False"
+            ]
+        },
+        
+        {
+            "category":"Science: Mathematics",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"L&#039;H&ocirc;pital was the mathematician who created the homonymous rule that uses derivatives to evaluate limits with indeterminations.",
+            "correct_answer":"False",
+            "incorrect_answers":[
+                "True"
+            ]
+        },
+        
+        {
+            "category":"Entertainment: Music",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"Pete Townshend&#039;s solo album, &quot;White City: A Novel&quot;, is set in the metropolitan area of Chicago.",
+            "correct_answer":"False",
+            "incorrect_answers":[
+                "True"
+            ]
+        },
+        
+        {
+            "category":"Entertainment: Japanese Anime & Manga",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"The character Plum from &quot;No Game No Life&quot; is a girl.",
+            "correct_answer":"False",
+            "incorrect_answers":[
+                "True"
+            ]
+        },
+        
+        {
+            "category":"Science: Computers",
+            "type":"boolean",
+            "difficulty":"hard",
+            "question":"The T-Mobile Sidekick smartphone is a re-branded version of the Danger Hiptop.",
+            "correct_answer":"True",
+            "incorrect_answers":[
+                "False"
+            ]
+        }
+    ]
+}
---
+++ src/components/QuizScreen.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import CategoryTitle from './../components/CategoryTitle'
+import Cards from './../components/Cards'
+import AnswerButtons from './../components/AnswerButtons'
+
+import React from 'react'
+
+const QuizScreen = () => {
+  return (
+    <>
+        <CategoryTitle/>
+        <Cards/>
+        <AnswerButtons/>
+    </>
+  )
+}
+
+export default QuizScreen
