43e080c5164805cb1a491ea7ab063e19b806d540
ruedalibre <andresruedalibre@gmail.com>
2022-03-30 00:36:47 -0500

    styles part I

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,17 @@ const App = () => {
     }
     
     return (
-    <>
         <div className='main-container'>
             <div className='static-text'>
                 <h1>Welcome to the Trivia Challenge!</h1>
-                <p>You will be presented with 11 True or False Questions</p>
+                <p>You will be presented with <br /> 
+                Eleven True or False Questions</p>
                 <p>Can You score 100%?</p>
             </div>
-            <div>
+            <div className='button-container'>
                 <button onClick={beginQuiz}>BEGIN</button>
             </div>
         </div>
-    </>
     )
 }
 
--- src/components/QuizScreen.jsx
+++ src/components/QuizScreen.jsx
@@ -2,18 +2,23 @@ import React from "react"
 import CategoryTitle from '../elements/CategoryTitle'
 import QuestionCards from '../elements/QuestionCards'
 import { TrueButton, FalseButton } from "../elements/Button"
-
-import React from 'react'
+import './QuizScren.css'
 
 const QuizScreen = () => {
   return (
-    <>
+    <div className="quiz-container">
+      <div className="quiz-categories-title">
         <CategoryTitle category='Entertainment' color='green'/>
         <SubcategoryTitle subcategory='Video Games' color='blue'/>
+      </div>
+      <div className="cards-container">
         <QuestionCards/>
+      </div>
+      <div className="answer-button-container">
         <TrueButton/>
         <FalseButton/>
-    </>
+      </div>
+    </div>
   )
 }
 
